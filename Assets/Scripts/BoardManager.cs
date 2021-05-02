b615d62588aca1d8ac8e31091c2e49e7c8921353
Yukito-F <yukito.izuminohara@gmail.com>
2021-05-02 13:12:31 +0900

    2021_05_02_update

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,45 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BoardManager : MonoBehaviour
 {
     public int[,] boardInfo = new int[8, 8];
-    private GameObject[,] discInfo = new GameObject[8,8];
+    private GameObject[,] discList = new GameObject[8,8];
 
     // 1:Black 0:void -1:White
     private void Start()
     {
         GameObject prefab = Resources.Load("Disc") as GameObject;
-        for (int i = 0; i < 64; i++)
+        for (int i = 0; i < 8; i++)
         {
-            int row = (int)i / 8;
-            int column = i % 8;
-            // boardInfo[row, column] = 0;
-            boardInfo[row, column] = 1 - 2 * (i % 2);
-            discInfo[row, column] = 
-                Instantiate(prefab, 
-                            new Vector3(-3.5f + i % 8, 1, -3.5f +(int)i / 8), 
-                            Quaternion.identity);
+            for (int j = 0; j < 8; j++)
+            {
+                boardInfo[i, j] = 1;
+                discList[i, j] = Instantiate(prefab,
+                                             new Vector3(-3.5f + i, 1, -3.5f + j),
+                                             Quaternion.identity);
+            }
         }   
     }
 
     private void Update()
     {
-        for (int i = 0; i < 64; i++)
+        for (int i = 0; i < 8; i++)
         {
-            int cell = boardInfo[(int)i / 8, i % 8];
-            GameObject disc = discInfo[(int)i / 8, i % 8];
-            if (cell == 0)
+            for (int j = 0; j < 8; j++)
             {
-                if (disc.activeSelf) disc.SetActive(false);
+                int state = boardInfo[i, j];
+                GameObject disc = discList[i, j];
+                if (state == 0)
+                {
+                    if (disc.activeSelf) disc.SetActive(false);
+                }
+                else
+                {
+                    if (!disc.activeSelf) disc.SetActive(true);
+                }
+                disc.GetComponent<Disc>().reload(state);
             }
-            else
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            for (int i = 0; i < 8; i++)
             {
-                if (!disc.activeSelf) disc.SetActive(true);
-                Vector3 angles = Vector3.zero;
-                if (cell == -1) angles.x = 180;
-                disc.transform.localEulerAngles = angles;
+                for (int j = 0; j < 8; j++)
+                {
+                    boardInfo[i, j] = Random.Range(-1, 1);
+                }
             }
         }
     }
